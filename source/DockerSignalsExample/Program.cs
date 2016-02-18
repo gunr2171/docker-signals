00ce8ab03c530bcde8cbef5ce6afafade2eddb00
Kevin Hunter <kevin.hunter@sylvania.com>
2016-02-18 16:47:13 -0500

    almost done with code

--- source/DockerSignalsExample/Program.cs
+++ source/DockerSignalsExample/Program.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DockerSignalsExample
 {
     class Program
     {
+        /// <summary>
+        /// waithandle for shutdown
+        /// </summary>
+        static ManualResetEvent shutdownEvent = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Program start");
+
+            var sideThread = new Thread(LoopDateToConsole);
+            sideThread.Start();
+
+            // wait to get signaled
+            // we do it this way because this is cross-thread
+            shutdownEvent.WaitOne();
+        }
+
+        private static void LoopDateToConsole()
+        {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                Thread.Sleep(2000);
+            }
         }
     }
 }
