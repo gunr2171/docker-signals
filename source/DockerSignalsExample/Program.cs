1700b3fe80f67f765362379fbf83f752830d9bdb
Kevin Hunter <kevin.hunter@sylvania.com>
2016-02-18 16:49:04 -0500

    should stop nicely on control c

--- source/DockerSignalsExample/Program.cs
+++ source/DockerSignalsExample/Program.cs
@@ -18,12 +18,21 @@ namespace DockerSignalsExample
         {
             Console.WriteLine("Program start");
 
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
             var sideThread = new Thread(LoopDateToConsole);
             sideThread.Start();
 
             // wait to get signaled
             // we do it this way because this is cross-thread
             shutdownEvent.WaitOne();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("Shutdown order given.");
+            // signal threads that wait for this
+            shutdownEvent.Set();
         }
 
         private static void LoopDateToConsole()
